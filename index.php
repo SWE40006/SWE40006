6f86a08c619e1752d606c3f6e88bdeb04c8b63ad
Stefan Kumarasinghe <stefankumarasinghe@gmail.com>
2023-10-10 02:12:58 +1100

    Should be deployed

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World, Stefan is a coder</p>'; ?>
+        <?php echo '<p>I am code</p>'; ?>
     </body>
 </html>
