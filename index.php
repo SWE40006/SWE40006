8174d7375975493bbb874036bb772aa7fe6ea3f0
Stefan Kumarasinghe <stefankumarasinghe@gmail.com>
2023-10-10 23:18:53 +1100

    Evolution animation

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Evolution Animation</title>
     <style>
         body {
+            font-family: 'Quicksand', sans-serif;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -67,8 +71,10 @@
     </style>
 </head>
 <body class="bg-dark p-5">
+    <div>
     <h1 class="text-white">The Evolution of Software</h1>
-    <p>By Stefan , Joe, Tevy, Wei and Mikkel</p>
+    <p class="text-white">By Stefan , Joe, Tevy, Wei and Mikkel</p>
+    </div>
     <div class="container text-center evolution-container">
         <!-- Monkey -->
         <div class="evolution-stage monkey">
