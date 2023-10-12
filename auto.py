ac01e0437257eb3c02a9be7b47c7c499e1b8e737
Stefan Kumarasinghe <stefankumarasinghe@gmail.com>
2023-10-12 14:22:04 +1100

    s - 1

--- auto.py
+++ auto.py
@@ -1,41 +1,37 @@
 import os
 import subprocess
 
-from anyio import sleep
-
 commit_message = ""
 print("######################### Automated Script Created by Stefan - All rights Reserved #########################")
-purpose = str(input("What was the change made to it, Please be as descriptive as possible as it helps others know what you were working on :    "))
-commit_message +=purpose
-auth = str(input("Please input your credentials or ID, This will be visible to everybody :    "))
-# Navigate to the directory of the current script
+purpose = input("What was the change made to it? Please be as descriptive as possible as it helps others know what you were working on:    ")
+commit_message += purpose
+
+auth = input("Please input your credentials or ID (This will be visible to everybody):    ")
 commit_message += " - " + auth
+
+# Navigate to the directory of the current script
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-# Add all changes
-print("######################### You are retrieving the latest resources #########################")
-output = subprocess.check_output(['git', 'checkout', 'production']).decode()
-# Add all changes
-output = subprocess.check_output(['git', 'add', '.']).decode()
+
+# Pull the latest changes from the production branch
+print("######################### Retrieving the latest resources #########################")
+output = subprocess.check_output(['git', 'add', '.']).decode() # Adding changes
 print(output)
-# Commit changes
-output = subprocess.check_output(['git', 'commit', '-m', commit_message]).decode()
+output = subprocess.check_output(['git', 'commit', '-am', commit_message]).decode()  # Committing changes
+print(output)
+
+output = subprocess.check_output(['git', 'checkout', 'production']).decode()
+print(output)
+
 output = subprocess.check_output(['git', 'pull']).decode()
 print(output)
-# Add all changes
-output = subprocess.check_output(['git', 'add', '.']).decode()
-print(output)
-# Commit changes
-output = subprocess.check_output(['git', 'commit', '-m', commit_message]).decode()
 
-print(output)
-choice = str(input("Would you like to push it to the main server, note the default branch is main, if you would like to change it type production :  Y/N "))
-# Commit changes
-if (choice == "Y" or choice =="y"):
-  print("######################### Your GitHub is now pushing to the main. There might be merge conflicts and errors that could fail the tests so make sure you are ready to handle them or revert back #########################")
-  output = subprocess.check_output(['git', 'push']).decode()
-  print(output)
-elif ((choice).lower()=="production") :
-   output = subprocess.check_output(['git', 'checkout', 'production']).decode()
-   print("######################### WARNING ! You are pushing to the production server. Be careful since it might be deployed to azure. Errorneous files will not be deployed but will be committed to the branch so please revert them #########################")
-   output = subprocess.check_output(['git', 'push']).decode()
-   print(output)
+choice = input("Would you like to push it to the server? The default branch is main. If you would like to change it, type 'production': Y/N ").lower()
+
+if choice == 'y':
+    print("######################### Your GitHub is now pushing to the main. There might be merge conflicts and errors that could fail the tests, so make sure you are ready to handle them or revert back #########################")
+    output = subprocess.check_output(['git', 'push']).decode()
+    print(output)
+elif choice == 'production':
+    print("######################### WARNING! You are pushing to the production server. Be careful since it might be deployed to Azure. Erroneous files will not be deployed but will be committed to the branch, so please revert them if necessary #########################")
+    output = subprocess.check_output(['git', 'push']).decode()
+    print(output)
