76f77cf6bf956b7f660234dca01e94e3c17dd6e3
Stefan Kumarasinghe <stefankumarasinghe@gmail.com>
2023-10-12 13:58:55 +1100

    Scripts - 103804645

--- auto.py
+++ auto.py
@@ -13,22 +13,28 @@ commit_message += " - " + auth
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Add all changes
 print("######################### You are retrieving the latest resources #########################")
-output = subprocess.check_output(['git', 'checkout', 'main']).decode()
-output = subprocess.check_output(['git', 'pull']).decode()
-print(output)
-# Add all changes
+
 output = subprocess.check_output(['git', 'add', '.']).decode()
 print(output)
-# Commit changes
 output = subprocess.check_output(['git', 'commit', '-m', commit_message]).decode()
 print(output)
-choice = str(input("Would you like to push it to the main server, note the default branch is main, if you would like to change it type production :  Y/N "))
-# Commit changes
-if (choice == "Y" or choice =="y"):
+output = subprocess.check_output(['git', 'checkout', 'production']).decode()
+output = subprocess.check_output(['git', 'pull']).decode()
+print(output)
+
+choice = str(input("Would you like to continue further :  Y/N  "))
+if (choice == "Y"):
+ 
+ # Add all changes
+
+ # Commit changes
+ choice = str(input("Would you like to push it to the main server, note the default branch is main, if you would like to change it type production :  Y/N "))
+ # Commit changes
+ if (choice == "Y" or choice =="y"):
   print("######################### Your GitHub is now pushing to the main. There might be merge conflicts and errors that could fail the tests so make sure you are ready to handle them or revert back #########################")
   output = subprocess.check_output(['git', 'push']).decode()
   print(output)
-elif ((choice).lower()=="production") :
+ elif ((choice).lower()=="production") :
    output = subprocess.check_output(['git', 'checkout', 'production']).decode()
    print("######################### WARNING ! You are pushing to the production server. Be careful since it might be deployed to azure. Errorneous files will not be deployed but will be committed to the branch so please revert them #########################")
    output = subprocess.check_output(['git', 'push']).decode()
