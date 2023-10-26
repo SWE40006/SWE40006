bf9cb3928cbb7d9c8b44c3590f8baface75776bf
Stefan Kumarasinghe <stefankumarasinghe@gmail.com>
2023-10-26 15:21:52 +1100

    Adding a new change  - 103804645

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ $date = new DateTime();
 ?>
 <body class="bg-dark p-5">
     <div>
-        <h1 class="text-white"><?php echo($date); ?></h1>
+    <h1 class="text-white"><?php echo $date->format('Y-m-d H:i:s'); ?></h1>
         <p class="text-white">By Stefan, Joe, Tevy, Wei, Mikkel</p>
         <p>Testing</p>
     </div>
